430ed859726b466abdd38bcb1e2a65ef1df153a5
sanjay <devarasettysanjay9229@gmail.com>
2021-01-08 20:12:52 +0800

    Added code for Jwt token Generation

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,12 +5,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.example.demo.config.JwtUtil;
+
+import io.jsonwebtoken.Claims;
+
 @SpringBootApplication
 @ComponentScan("com.example.demo")
 @EnableCaching
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		
+		
+		//Checking whether token is generated or not and testing
+		JwtUtil util=new JwtUtil();
+		String token=util.generateToken("saa", "sanjay", "sanju");
+		System.out.println(token);
+		
+	Claims claims=	util.getClaims("sanju", token);
+	System.out.println(claims.getSubject());
+	
+	
+	
+		
+		
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
